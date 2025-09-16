3362f775d6b15362d9f56d1f0fb450ef705dc373
Rita <ritadosreis83@gmail.com>
2025-09-16 06:24:01 -0300

    testes

--- test/tranferenciaTest.js
+++ test/tranferenciaTest.js
@@ -2,48 +2,40 @@ const request = require('supertest');
 const { expect } = require('chai');
 require('dotenv').config();
 const { obterToken } = require('../helpers/autenticacao');
+const path = require('path');
 const postTransferencia = require('../fixture/postTransferencia.json');
 
-describe('Transferencias', () => {
+describe('Transferencias', function () {
   let token;
 
   before(async function () {
-
     token = await obterToken('Julio.Lima', '123456');
     expect(token, 'token inválido no login').to.be.a('string').and.not.empty;
+    expect(process.env.BASE_URL, 'BASE_URL não definida no .env').to.be.a('string').and.not.empty;
   });
 
-  describe('POST /transferencias', () => {
-    it('deve retornar 201 quando valor >= 10,00', async () => {
-        const transferencias = {...postTransferencia};
-
-      const response = await request(process.env.BASE_URL)
+  describe('POST /transferencias', function () {
+    it('deve retornar 201 quando valor >= 10,00', async function () {
+      const bodyTransferencias = { ...postTransferencia };
+      const res = await request(process.env.BASE_URL)
         .post('/transferencias')
         .set('Content-Type', 'application/json')
         .set('Authorization', `Bearer ${token}`)
-        .send(bodytransferencias);
+        .send(bodyTransferencias);
 
-        });
+      expect(res.status).to.equal(201);
 
-      expect(
-        response.status,
-        `status inesperado. Body: ${JSON.stringify(response.body)}`
-      ).to.equal(201);
     });
 
-    it('deve retornar 422 quando valor < 10,00', async () => {
-        const transferencias = {...postTransferencia};
-        body.transferencias.valor = 7;
-      const response = await request(process.env.BASE_URL)
+    it('deve retornar 422 quando valor < 10,00', async function () {
+      const bodyTransferencias = { ...postTransferencia, valor: 7 };
+      const res = await request(process.env.BASE_URL)
         .post('/transferencias')
         .set('Content-Type', 'application/json')
         .set('Authorization', `Bearer ${token}`)
-        .send(transferencias);
+        .send(bodyTransferencias);
 
-      expect(
-        response.status,
-        `status inesperado. Body: ${JSON.stringify(response.body)}`
-      ).to.equal(422);
+      expect(res.status).to.equal(422);
     });
   });
-
+});
