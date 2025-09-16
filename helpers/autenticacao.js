0910971438b9f85316033d07ebbaa1c1290b8d30
Rita <ritadosreis83@gmail.com>
2025-09-16 05:59:42 -0300

    pasta fixture

--- helpers/autenticacao.js
+++ helpers/autenticacao.js
@@ -1,16 +1,17 @@
 const request = require('supertest')
+const postLogin = require('../fixture/postLogin.json')
 
 const obterToken = async (usuario, senha) => {
+
+    const bodyLogin = { ...postLogin }
     const respostaLogin = await request(process.env.BASE_URL)
         .post('/login')
         .set('Content-Type', 'application/json')
-        .send({
-            'username': usuario,
-            'senha': senha
-        })
+        .send(bodyLogin
+        )
     return respostaLogin.body.token
 }
 
 module.exports = {
     obterToken
-}   
+}
--- test/tranferenciaTest.js
+++ test/tranferenciaTest.js
@@ -2,46 +2,43 @@ const request = require('supertest');
 const { expect } = require('chai');
 require('dotenv').config();
 const { obterToken } = require('../helpers/autenticacao');
+const postTransferencia = require('../fixture/postTransferencia.json');
 
 describe('Transferencias', () => {
   let token;
 
   before(async function () {
 
-    token = await obterToken('Julio.Lima', '123456'); // corrigido
+    token = await obterToken('Julio.Lima', '123456');
     expect(token, 'token inválido no login').to.be.a('string').and.not.empty;
   });
 
   describe('POST /transferencias', () => {
     it('deve retornar 201 quando valor >= 10,00', async () => {
+        const transferencias = {...postTransferencia};
+
       const response = await request(process.env.BASE_URL)
         .post('/transferencias')
         .set('Content-Type', 'application/json')
         .set('Authorization', `Bearer ${token}`)
-        .send({
-          contaOrigem: 1,
-          contaDestino: 2,
-          valor: 11,
-          token: ''
+        .send(bodytransferencias);
+
         });
 
       expect(
         response.status,
         `status inesperado. Body: ${JSON.stringify(response.body)}`
-      ).to.equal(201); 
+      ).to.equal(201);
     });
 
     it('deve retornar 422 quando valor < 10,00', async () => {
+        const transferencias = {...postTransferencia};
+        body.transferencias.valor = 7;
       const response = await request(process.env.BASE_URL)
         .post('/transferencias')
         .set('Content-Type', 'application/json')
         .set('Authorization', `Bearer ${token}`)
-        .send({
-          contaOrigem: 1,
-          contaDestino: 2,
-          valor: 7,
-          token: ''
-        });
+        .send(transferencias);
 
       expect(
         response.status,
@@ -49,4 +46,4 @@ describe('Transferencias', () => {
       ).to.equal(422);
     });
   });
-});
+
